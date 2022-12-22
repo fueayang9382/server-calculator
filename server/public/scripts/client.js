098e3c014c7fa5b048522b288b74c7b4b1d928ef
Tou Fue Yang <touyangbiz@gmail.com>
2022-12-21 18:59:31 -0600

    get route works

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,3 +1,5 @@
+//const { response } = require("express");
+
 console.log('is JQ loaded?');//yes it is loaded.
 
 $(document).ready(mathInputCompute);
@@ -6,10 +8,25 @@ let operator = "";
 function mathInputCompute() {
     $('#submitTwoNumbers').on('click', theSubmitTwoNums);//make button click. // YES it is working
     $('#clearTheResult').on('click', clearTheResult);//make the clear button click. YEs it is working
-    $('.button').on('click', function() {
-        Operator = $(this).html();
+    $('#add').on('click', function () {
+        operator = '+'
+        console.log('this is my operator', operator);
     })
-    $('#submitTwoNumbers').on('click', resultGetRoute);
+   $ ('#subtract').on('click', function () {
+        operator = '-'
+        console.log('this is my operator', operator);
+    })
+    $('#divide').on('click', function () {
+        operator = '/'
+        console.log('this is my operator', operator);
+    })
+    $('#multiply').on('click', function () {
+        operator = '*'
+        console.log('this is my operator', operator);
+    })
+
+    fetchAndRenderCalculation()
+    // $('#submitTwoNumbers').on('click', resultGetRoute);
     // $('#addingTwoNums').on('click', theAdditionButton)
     //the clear button might need a 'body instead. maybe not since server 
     //might clear it from there. 
@@ -19,74 +36,51 @@ function mathInputCompute() {
 //capture value, assign value to object, post 
 function theSubmitTwoNums() {
     console.log('We are in the submit button!');
-//have the submit button capture those two numbers as value. 
-let inputOne = $('#numOne').val();
-let inputTwo = $('#numTwo').val();
-//the capture values will be assign as object: num1 and num2.
-let twoNumberObject ={
+    //have the submit button capture those two numbers as value. 
+    let inputOne = $('#numOne').val();
+    let inputTwo = $('#numTwo').val();
+    if(!inputOne || !inputTwo || !operator){
+        alert('Please try again. Make sure to enter mathematical equation')
+        return;
+    }
+    //the capture values will be assign as object: numberX1 and numberX2.
+    let twoNumberObject ={
     numberX1: inputOne,
     numberX2: inputTwo,
     operator: operator
-}//these values in the object will be post on the server side.
-//They will wait for further instruction. 
-// this is a POST route;therefore,result/get will be on the GET route.
-// Nothing will append here. 
-$.ajax({
-    url: '/sendNumbersObjectToServer',
-    method: 'post', //sending the object to the server
-    data: twoNumberObject
-}).then((res)=>{
-    console.log(res);
-})
-}
-
-function resultGetRoute() {
+    }//these values in the object will be post on the server side.
+    //They will wait for further instruction. 
+    // this is a POST route;therefore,result/get will be on the GET route.
+    // Nothing will append here. 
     $.ajax({
-  url: '/displayResult',
-  method: 'GET'
-}).then((Response) => {
-  console.log('server sent us:', Response);
-  $('#recent').empty()
-  for (let solution of Response) {
-    $('#result').empty()
-    $('#result').append(`${solution.result}`)
-    $('#recent').append(`
-   <tr>
-      <td>${solution.numberX1} ${solution.operator} ${solution.numberX2} = ${solution.result}</td>
-   </tr>
-    `)
-  }
-})
+    url: '/calculations',
+    method: 'POST', //sending the object to the server
+    data: twoNumberObject
+    }).then((res)=>{
+    console.log(res);
+    })
 }
 
-
-    //needs to be send to the server side. from there the math 
-    //function can take place. 
-    //
-
-
-function submissionDenied() {
+function checkForSubmitionDenied() {
     if (numberX1 === '' || numberX2 === '' || operator === '') {
-        console.log('Please try again. Make sure to enter mathematical equation');
+        alert('Please try again. Make sure to enter mathematical equation')
         return; 
     }
-}// this function make sure users have correct full math equation.
-
-
-
-
-//send a post object to the server.  -Done
-//create an empty global array on the server side. -Done
-// when the object is in the server side, push the object into the empty global array. -Done
-// make the server send a post respond to indicate that it received the instruction. -Done
-// ^^^ the steps above are done. 
-
-//Make a  GET route that will retrieve the array back and append as history( string)
-//The same GET route should also retrieve the math result, append on the DOM. 
-//The clear button should empty the result. 
-
-
-//HINT: this is possible to just have ONE POST route and ONE GET ROUTE: dont make this more 
-//confusing than it needs to be.
-//the submit button should do most of the work. the clear button should empty the result 
-// The history can only go away if the server is restarted. 
+}
+// this function make sure users have correct full math equation.
+//get data from / calculation , then render
+// it on the dom
+function fetchAndRenderCalculation() {
+    $.ajax({
+        method: 'GET',
+        url: '/calculations'
+    }).then((response)=>{
+        console.log(response);
+        $('#recentHistory').empty();
+        for (let aCal of response){
+        $('#recentHistory').append(`
+        <li>${aCal.numOne} ${aCal.operator} ${aCal.numTwo} = ${aCal.result}</li>
+        
+        `)}
+    })
+}
--- server/server.js
+++ server/server.js
@@ -1,59 +1,43 @@
-//create an global empty array: make sure you dont mix up the name.
-theCalculationHistory = [];
 
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
-const PORT = 5004;
+const PORT = 5000;
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static('server/public'));
+app.use(bodyParser.json())
+
+//dummy data to send back to the client --> app.get
+let calculations = [
+{
+    numOne: 8,
+    numTwo: 10,
+    operator: '+',
+    result: 18
+},
+{
+    numOne: 100,
+    numTwo: 50,
+    operator: '+',
+    result: 50
+}
+]
+
+// send the calculation to the client. 
+app.get('/calculations', (req,res)=>{
+    console.log(calculations);
+    res.send(calculations)//why cant I send it back to client. 
+})
+
+
+app.post('/calculations', (req, res)=>{
+    console.log(req.body);
+    
+})
+
+
+
+
 app.listen(PORT, () => {
     console.log ('Server is running on port', PORT)
 })
-let finalResult = [];
-let twoNumbersObject = []; 
-app.post('/sendNumbersObjectToServer',(req, res)=> {
-    console.log('In the step to push object into the array.');
-    let numberX1 = req.body.numberX1
-    let numberX2 = req.body.numberX2
-    let operator = req.body.operator
-
-
-    switch (operator){
-        case "+":
-            result= Number(numberX1) + Number(numberX2);
-            break;
-        case "-":
-            result= Number(numberX1) - Number(numberX2);
-            break;
-        case "/":
-            result= Number(numberX1) / Number(numberX2);
-            break;
-        case "*":
-            result= Number(numberX1) * Number(numberX2);
-            break;
-    }
-
-    twoNumbersObject = {
-        numberX1: numberX1,
-        numberX2: numberX2,
-        operator: operator,
-        result: result
-    }
-
-    finalResult.push(twoNumbersObject);
-
-    res.send(201);
-})
-
-
-app.get('/displayResults', (req, res) => {
-    res.send(finalResult)
-})
-//^the function pushed the object into the array and the server
-// respond back stating it did the command I told it to do. 
-
-
-
-
-
