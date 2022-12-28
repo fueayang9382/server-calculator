ba76c1d8b7f147f5cf24187a645962e227a578d1
Tou Fue Yang <touyangbiz@gmail.com>
2022-12-27 19:38:56 -0600

    got my clear button to work

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,72 +1,67 @@
 //const { response } = require("express");
 
-console.log('is JQ loaded?');//yes it is loaded.
+console.log('is JQ loaded?'); //yes it is loaded.
 
 $(document).ready(mathInputCompute);
-let operator = "";
+let operator = '';
 //this is the rendering function.
 function mathInputCompute() {
-    $('#submitTwoNumbers').on('click', theSubmitTwoNums);//make button click. // YES it is working
-    $('#clearTheResult').on('click', clearTheResult);//make the clear button click. YEs it is working
+    $('#submitTwoNumbers').on('click', theSubmitTwoNums); //make button click. // YES it is working
+    $('body').on('click', '.clearTheResult ', clearTheResult); //make the clear button click. YEs it is working
     $('#add').on('click', function () {
-        operator = '+'
+        operator = '+';
         console.log('this is my operator', operator);
-    })
-   $ ('#subtract').on('click', function () {
-        operator = '-'
+    });
+    $('#subtract').on('click', function () {
+        operator = '-';
         console.log('this is my operator', operator);
-    })
+    });
     $('#divide').on('click', function () {
-        operator = '/'
+        operator = '/';
         console.log('this is my operator', operator);
-    })
+    });
     $('#multiply').on('click', function () {
-        operator = '*'
+        operator = '*';
         console.log('this is my operator', operator);
-    })
+    });
 
-    fetchAndRenderCalculation()
-    // $('#submitTwoNumbers').on('click', resultGetRoute);
-    // $('#addingTwoNums').on('click', theAdditionButton)
-    //the clear button might need a 'body instead. maybe not since server 
-    //might clear it from there. 
+    fetchAndRenderCalculation();
 }
 
-
-//capture value, assign value to object, post 
+//capture value, assign value to object, post
 function theSubmitTwoNums() {
     console.log('We are in the submit button!');
-    //have the submit button capture those two numbers as value. 
+    //have the submit button capture those two numbers as value.
     let inputOne = $('#numOne').val();
     let inputTwo = $('#numTwo').val();
-    if(!inputOne || !inputTwo || !operator){
-        alert('Please try again. Make sure to enter mathematical equation')
+    if (!inputOne || !inputTwo || !operator) {
+        alert('Please try again. Make sure to enter mathematical equation');
         return;
     }
     //the capture values will be assign as object: numberX1 and numberX2.
-    let twoNumberObject ={
-    numberX1: inputOne,
-    numberX2: inputTwo,
-    operator: operator
-    }//these values in the object will be post on the server side.
-    //They will wait for further instruction. 
+    let twoNumberObject = {
+        numberX1: inputOne,
+        numberX2: inputTwo,
+        operator: operator,
+    }; //these values in the object will be post on the server side.
+    //They will wait for further instruction.
     // this is a POST route;therefore,result/get will be on the GET route.
-    // Nothing will append here. 
+    // Nothing will append here.
     $.ajax({
-    url: '/calculations',
-    method: 'POST', //sending the object to the server
-    data: twoNumberObject
-    }).then((res)=>{
-    clearInputs()
-    fetchAndRenderCalculation()
-    console.log(res);
-    })
+        url: '/calculations',
+        method: 'POST', //sending the object to the server
+        data: twoNumberObject,
+    }).then((res) => {
+        clearInputs();
+        fetchAndRenderCalculation();
+        console.log(res);
+    });
 }
 
 function clearInputs() {
-    $('#numOne').val('')
-     $('#numTwo').val('')
-     operator = ''
+    $('#numOne').val('');
+    $('#numTwo').val('');
+    operator = '';
 }
 // this function make sure users have correct full math equation.
 //get data from / calculation , then render
@@ -74,14 +69,21 @@ function clearInputs() {
 function fetchAndRenderCalculation() {
     $.ajax({
         method: 'GET',
-        url: '/calculations'
-    }).then((response)=>{
+        url: '/calculations',
+    }).then((response) => {
         console.log(response);
         $('#recentHistory').empty();
-        for (let aCal of response){
-        $('#recentHistory').append(`
+        for (let aCal of response) {
+            $('#recentHistory').append(`
         <li>${aCal.numOne} ${aCal.operator} ${aCal.numTwo} = ${aCal.result}</li>
         
-        `)}
-    })
+        `);
+        }
+    });
+}
+
+function clearTheResult() {
+    console.log('in the clear histroy');
+    clearInputs()
+    //$(this).parent().remove()
 }
--- server/server.js
+++ server/server.js
@@ -1,71 +1,66 @@
-
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('server/public'));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 //dummy data to send back to the client --> app.get
 let calculations = [
-{
-    numOne: 8,
-    numTwo: 10,
-    operator: '+',
-    result: 18
-},
-{
-    numOne: 100,
-    numTwo: 50,
-    operator: '+',
-    result: 50
-}
-]
+    {
+        numOne: 8,
+        numTwo: 10,
+        operator: '+',
+        result: 18,
+    },
+    {
+        numOne: 100,
+        numTwo: 50,
+        operator: '+',
+        result: 50,
+    },
+];
 
-// send the calculation to the client. 
-app.get('/calculations', (req,res)=>{
+// send the calculation to the client.
+app.get('/calculations', (req, res) => {
     console.log(calculations);
-    res.send(calculations)//why cant I send it back to client. 
-})
+    res.send(calculations); //why cant I send it back to client.
+});
 
-
-app.post('/calculations', (req, res)=>{
+app.post('/calculations', (req, res) => {
     console.log(req.body);
-        console.log('In the step to push object into the array.');
-        let numberX1 = req.body.numberX1
-        let numberX2 = req.body.numberX2
-        let operator = req.body.operator
-        let result;
-        switch (operator){
-            case "+":
-                result= Number(numberX1) + Number(numberX2);
-                break;
-            case "-":
-                result= Number(numberX1) - Number(numberX2);
-                break;
-            case "/":
-                result= Number(numberX1) / Number(numberX2);
-                break;
-            case "*":
-                result= Number(numberX1) * Number(numberX2);
-                break;
-        }
-    
-       let  twoNumbersObject = {
-            numOne: numberX1,
-            numTwo: numberX2,
-            operator: operator,
-            result: result
-        }
-    
-        calculations.push(twoNumbersObject);
-        res.sendStatus(201);
-})
+    console.log('In the step to push object into the array.');
+    let numberX1 = req.body.numberX1;
+    let numberX2 = req.body.numberX2;
+    let operator = req.body.operator;
+    let result;
+    switch (operator) {
+        case '+':
+            result = Number(numberX1) + Number(numberX2);
+            break;
+        case '-':
+            result = Number(numberX1) - Number(numberX2);
+            break;
+        case '/':
+            result = Number(numberX1) / Number(numberX2);
+            break;
+        case '*':
+            result = Number(numberX1) * Number(numberX2);
+            break;
+    }
 
+    let twoNumbersObject = {
+        numOne: numberX1,
+        numTwo: numberX2,
+        operator: operator,
+        result: result,
+    };
 
-
+    calculations.push(twoNumbersObject);
+    res.sendStatus(201);
+});
 
 app.listen(PORT, () => {
-    console.log ('Server is running on port', PORT)
-})
+    console.log('Server is running on port', PORT);
+});
